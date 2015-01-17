888355b719c79dda376b8dcfc233a2457a378805
Ming LI <mli@pivotal.io>
2015-01-17 21:56:15 +0800

    initial version of my customization config for spf13-vim

---
+++ README.markdown
@@ -0,0 +1,15 @@
+
+
+** Navigation **
+YouCompleteMe
+<leader>jd  YcmCompleter GoToDefinitionElseDeclaration
+<leader>gd  YcmCompleter GoToDeclaration
+
+tag navigation:
+<C-]>, <C-w>], <leader>w]
+
+cscope navigation:
+,f (c|d|e|f|g|i|s|t)
+
+
+
---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Fetch absolute path to this script and current directory
+pushd . > /dev/null
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+  while([ -h "${SCRIPT_PATH}" ]) do
+    cd "`dirname "${SCRIPT_PATH}"`"
+    SCRIPT_PATH="$(readlink "`basename "${SCRIPT_PATH}"`")";
+  done
+cd "`dirname "${SCRIPT_PATH}"`" > /dev/null
+SCRIPT_PATH="`pwd`";
+popd  > /dev/null
+
+CURRENT_DIR=`pwd`
+
+################
+
+# Symlink the configuration files into their appropriate homes
+ln -sf $SCRIPT_PATH/.gvimrc.local ~/
+ln -sf $SCRIPT_PATH/.vimrc.before.fork ~/
+ln -sf $SCRIPT_PATH/.vimrc.bundles.fork ~/
+ln -sf $SCRIPT_PATH/.vimrc.fork ~/
+ln -sf $SCRIPT_PATH/.vimrc.local ~/
+ln -sf $SCRIPT_PATH/.ycm_extra_conf.py ~/
+
+vim +BundleInstall! +BundleClean +q
+
+
+
+
+
+################
+cd $CURRENT_DIR
